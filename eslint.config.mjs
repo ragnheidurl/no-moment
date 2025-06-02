fe6575796027911c46b554d03fa56c7d36cb670a
ragnheidurl <70703882+ragnheidurl@users.noreply.github.com>
2025-06-02 18:43:06 +0000

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,50 @@
-
+import tsParser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
-
-
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ["**/*.{ts,mts,cts}"],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: "latest", 
-        sourceType: "module", 
+        ecmaVersion: "latest",
+        sourceType: "module"
       },
     },
     rules: {
-      'no-restricted-imports': [
-           'error',
-           {
-             paths: [
-               {
-                 name: 'moment',
-                 message: 'Moment is not allowed',
-               },
-             ],
-           },
-         ],
+      "no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "moment",
+              message: "Moment is not allowed",
+            },
+          ],
+        },
+      ],
+    },
   },
-}]
-);
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "moment",
+              message: "Moment is not allowed",
+            },
+          ],
+        },
+      ],
+    },
+  },
+]);
